9bec6cc354604f6ebdc8a26be3d30919e382a84c
boyjo <iijokv@naver.com>
2023-05-25 20:58:55 +0900

    feat: Member 테이블 생성 확인

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,15 +14,7 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        // 문제 시 롤백을 위해 try-catch 사용
         try {
-
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("C");
-
-            // JPA를 통해 엔티티를 가져오면, JPA가 관리함.
-            // 트랜잭션 커밋 시, 엔티티 상태를 확인 -> 변경 확인 시 UPDATE 쿼리 전송
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
