31f35564b5cd0c9ec9df7ddb6b79ef32abbf761b
boyjo <iijokv@naver.com>
2023-05-26 13:27:15 +0900

    feat: 테이블 중심 모델링 예시

--- basic/src/main/java/hellojpa/JpaMain.java
+++ basic/src/main/java/hellojpa/JpaMain.java
@@ -15,9 +15,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("A");
 
+            //팀 저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            //회원 저장
+            Member member = new Member();
+            member.setName("member1");
+            member.setTeamId(team.getId());
             em.persist(member);
 
             tx.commit();
