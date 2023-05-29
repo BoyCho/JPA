4b45141a567d7531f1d51ca3f1079a2df5cf8e9e
boyjo <iijokv@naver.com>
2023-05-29 14:29:24 +0900

    feat: @MappedSuperclass 사용

--- shopApp/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ shopApp/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.items.Movie;
@@ -8,6 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -19,19 +21,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Movie movie = new Movie();
-            movie.setDirector("Director");
-            movie.setActor("Actor");
-            movie.setName("Movie");
-            movie.setPrice(1000);
+            Member member = new Member();
+            member.setCreatedBy("kim");
+            member.setLocalDateTime(LocalDateTime.now());
 
-            em.persist(movie);
+            em.persist(member);
 
             em.flush();
             em.clear();
-
-            em.find(Movie.class, movie.getId());
-            System.out.println("movie = " + movie.getName());
 
             tx.commit();
         } catch (Exception e) {
