511120067e7371227d28cde0fea3b8a61d8287ae
boyjo <iijokv@naver.com>
2023-05-29 11:18:52 +0900

    feat: 일대일

--- basic/src/main/java/hellojpa/Member.java
+++ basic/src/main/java/hellojpa/Member.java
@@ -11,20 +11,13 @@ public class Member {
     @Column(name = "USERNAME")
     private String name;
 
-    /*@Column(name = "TEAM_ID")
-    private Long teamId;*/
-
     @ManyToOne
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
 
     public String getName() {
         return name;
@@ -32,10 +25,6 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public Team getTeam() {
-        return team;
     }
 
     public void setTeam(Team team) {
