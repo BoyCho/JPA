a39ac80b1553c34916e59b4a70960dbeab1fdbe8
BoyCho <iijokv@naver.com>
2023-05-30 17:25:24 +0900

    feat: 즉시 로딩으로 인한 JPQL N+1 문제 예시

--- basic/src/main/java/hellojpa/JpaMain.java
+++ basic/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -27,8 +28,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            Member m = em.find(Member.class, member.getId());
-            System.out.println("m.getTeam().getClass() = " + m.getTeam().getClass());
+            List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
 
             tx.commit();
         } catch (Exception e) {
