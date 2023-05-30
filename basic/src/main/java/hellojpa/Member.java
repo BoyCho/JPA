e6876ebc971fff53e9aa68be5c98e300b310c76a
BoyCho <iijokv@naver.com>
2023-05-30 16:54:54 +0900

    feat: 지연 로딩 어노테이션 설정

--- basic/src/main/java/hellojpa/Member.java
+++ basic/src/main/java/hellojpa/Member.java
@@ -12,7 +12,7 @@ public class Member {
     @Column(name = "USERNAME")
     private String name;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
