0dfb1857bbb041f9d4467ce2b91d8eb3c1f1bcff
BoyCho <iijokv@naver.com>
2023-06-28 00:28:06 +0900

    feat: 파라미터 바인딩

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -1,9 +1,6 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,6 +15,13 @@ public class JpaMain {
             Member member = new Member();
             member.setUsername("member1");
             em.persist(member);
+
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+
+            System.out.println("result.getUsername() = " + result.getUsername());
+
 
             tx.commit();
         } catch (Exception e) {
