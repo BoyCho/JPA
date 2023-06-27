0c6741c733dd9ef7c7c7912ef65bafa6de9acc49
BoyCho <iijokv@naver.com>
2023-06-27 15:04:16 +0900

    setting

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -1,11 +1,9 @@
-package hellojpa;
+package jpql;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
-import java.util.Set;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,14 +15,9 @@ public class JpaMain {
         tx.begin();
 
         try {
-            List<Member> result = em.createQuery(
-                    "select m from Member m where m.name like '%kim%'",
-                    Member.class
-            ).getResultList();
-
-            for (Member member : result) {
-                System.out.println("member = " + member);
-            }
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
---
+++ jpql/src/main/java/jpql/Order.java
@@ -0,0 +1,45 @@
+package jpql;
+
+import javax.persistence.*;
+
+@Entity
+public class Order {
+
+    @Id @GeneratedValue
+    private Long id;
+    private int orderAmount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Product product;
+
+    @Embedded
+    private Address address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(int orderAmount) {
+        this.orderAmount = orderAmount;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
