4de828236489a0b63cde8be0164c0db6744ac6ce
BoyCho <iijokv@naver.com>
2023-06-28 17:00:31 +0900

    feat: 지연 로딩으로인한 N+1 문제 발생

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -1,8 +1,6 @@
 package jpql;
 
 import javax.persistence.*;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 public class JpaMain {
@@ -16,24 +14,41 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("team");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Member member1 = new Member("member1", 10, team);
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member("회원1", 10, teamA);
             em.persist(member1);
 
-            Member member2 = new Member("member2", 20, team);
+            Member member2 = new Member("회원2", 20, teamA);
             em.persist(member2);
+
+            Member member3 = new Member("회원3", 30, teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select mL.username from Team t left join t.members mL";
-            String result = em.createQuery(query, String.class)
-                    .getResultList().toString();
+            String query = "select m from Member m";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
 
-            System.out.println("result = " + result);
+            System.out.println("\n---------------------------\n");
+
+            // 현재는 team은 프록시 객체임
+            for (Member member : result) {
+                System.out.println("member.getUsername() = " + member.getUsername() + ", " + member.getTeam().getName());
+                // 회원1, 팀A(SQL)
+                // 회원2, 팀A(1차 캐시)
+                // 회원3, 팀B(SQL)
+                // => 총 쿼리가 3번 전송됨
+                // => 회원 N명 -> 최대 N + 1발생
+            }
 
             tx.commit();
         } catch (Exception e) {
